7c1ecc05b91f13dad9fde741871a293b62e14223
Khanh.Nguyen <khanh.nguyen@opower.com>
2014-09-29 03:11:10 -0400

    Refactor code using Matrix. Update formula to compute throughput

--- Scheduler.m
+++ Scheduler.m
@@ -1,69 +1,59 @@
 classdef Scheduler < handle
-    %% This is a cluster-base scheduler
-    %% Given a list of cells in a cluster, find the optimal configuration
+    %This is a cluster-base scheduler
+    %  Given a list of cells in a cluster, find the optimal configuration
     
+    %% Class properties
     properties
         utilityFunc;
         schedulingAlg;
-        M = 10;              % number of subframes in each frame
+        M;              % number of subframes in each frame
     end
     
+    %% Instance methods
     methods 
         function obj = Scheduler(M) 
+            %constructor sets the number of subframes
             obj.M = M;
         end
         
         function setUtilityFunction(obj, utilityFunc)
-            %% set utility function on the fly
+            %setUtilityFunction sets utility function
             obj.utilityFunc = utilityFunc;
         end
         
         function setSchedulingAlg(obj, alg)
-            %% set scheduling algorithm on the fly
+            %setSchedulingAlg sets scheduling algorithm
             obj.schedulingAlg = alg;
         end
         
-        function [mu, md, profit] = configure(obj, cells)
-            %% compute the optimal number of downlink and uplink
+        function [mu, md] = configure(obj, cells)
+            %configure computes the number of downlink and uplink subframes
             
             % get the uplinks and downlinks vector
-            [uplinks, downlinks] = Scheduler.getRequirements(cells);
+            uplinks = cells.CellMatrix(:,2);
+            downlinks = cells.CellMatrix(:,3);
             
-            % compute the subframe-base profits for each cells
-            % get the dProfits and uProfits vector
+            % compute the subframe-base profits
             [uProfits, dProfits] = Scheduler.computeSubframeProfits(cells, obj.utilityFunc);
             
             % schedule 
-            [mu, md, profit] = obj.schedulingAlg.schedule(uplinks, downlinks, uProfits, dProfits, obj.M);
+            [mu, md] = obj.schedulingAlg.schedule(uplinks, downlinks, uProfits, dProfits, obj.M);
         end
     end
     
+    %% Static methods
     methods (Static)
-        function [uplinks, downlinks] = getRequirements(cells) 
-            %% get number of uplinks and downlinks required for each cells
-            uplinks = zeros(1, length(cells));
-            downlinks = zeros(1, length(cells));
-            idx = 1;
-            for cell = cells
-                %uplinks(idx) = cell.uplink;
-                %downlinks(idx) = cell.downlink;
-                uplinks(idx) = cell.uQueue;
-                downlinks(idx) = cell.dQueue;
-                idx = idx + 1;
-            end
+        function [uProfits, dProfits] = computeSubframeProfits(cells, utilityFunc) 
+            profits = utilityFunc(cells);
+            uProfits = profits(:,1);
+            dProfits = profits(:,2);
         end
         
-        function [uProfits, dProfits] = computeSubframeProfits(cells, utilityFunc) 
-            uProfits = zeros(1, length(cells));
-            dProfits = zeros(1, length(cells));
-            idx = 1;
-            
-            for cell = cells
-                [uP, dP] = utilityFunc(cell);
-                uProfits(idx) = uP;
-                dProfits(idx) = dP;
-                idx = idx + 1;
-            end
+        function b = needReconfiguration()
+            %needReconfiguration returns true if cluster needs to be
+            %  reconfigured, false otherwise.
+            %  For simplicity, we reconfigure for every frame
+            b = true;
         end
     end
 end
--- UtilityFunctions.m
+++ UtilityFunctions.m
@@ -1,39 +1,20 @@
 classdef UtilityFunctions 
-    %UtilityFunctions contains static functions that take a Cell as input
-    %  an return the downlink and uplink profits for that cell
+    %UtilityFunctions computes the downlink and uplink profits for cells
     % 
-    %  FIXME: is `profit` the right terminology? If not, what's the right
-    %  one?
     methods (Static)
-        function [uProfit, dProfit] = simpleUtilityFunc(cell)
-            %simpleUtility returns profits from a fixture table. (used for
-            %  testing)
-            load('data/fixtureData.mat');
-            id = cell.cellId;
-            uProfit = fixtureData(id,1);
-            dProfit = fixtureData(id,2);
-        end
-        
-        function [uProfit, dProfit] = queueLengthBaseFunc(cell)
-            %queueLengthBaseFunc defines utility solely based on queue
-            %  length
-            uProfit = cell.uQueue;
-            dProfit = cell.dQueue;
-        end
-        
-        function [uProfit, dProfit] = linearQueueLength(cell)
+        function profits = linearQueueLength(cells)
             %linearQueueLength utility proportional to queue length
             
-            uProfit = cell.uThroughput() * cell.uQueue;
-            dProfit = cell.qThroughput() * cell.dQueue;
+            avgThroughput = cells.getAvgThrouhgput();
+            profits = cells.CellMatrix(:,4:5) ./ avgThroughput;
         end
         
-        function [uProfit, dProfit] = expQueueLength(cell)
+        function profits = expQueueLength(cell)
             %expQueueLength utility proportional to the exponential of 
             %  queue length
             
-            uProfit = cell.uThroughput * exp(cell.uQueue);
-            dProfit = cell.dThroughput * exp(cell.dQueue);
+            avgThroughput = cells.getAvgThrouhgput();
+            profits = exp(cell.CellMatrix(:,4:5)) ./ avgThroughput;
         end
     end
 end
