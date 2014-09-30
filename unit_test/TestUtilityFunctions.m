5459744e1e120caf068ead2c824803c532660946
Khanh.Nguyen <khanh.nguyen@opower.com>
2014-09-29 20:53:05 -0400

    Add unit test for UtitlityFunctions.m

--- unit_test/TestUtilityFunctions.m
+++ unit_test/TestUtilityFunctions.m
@@ -2,13 +2,29 @@ classdef TestUtilityFunctions < matlab.unittest.TestCase
     %TestUtilityFunctions unit-tests UtilityFunctions
     
     properties
+        cells
     end
     
-    methods
-        function testLinearQueue( testCase )
+    methods (TestMethodSetup)
+        function setup(testCase)
+            disp('settting up cells');
+            testCase.cells = Cells(3,10);
+            testCase.cells.setDataRate([10 20; 30 30; 40 20]);
+            testCase.cells.setDemand([2 7; 3 6; 1 9]);
+        end
+    end
+    
+    methods (Test)
+        function testDataRateBase( testCase )
+            actual = UtilityFunctions.dataRateBase(testCase.cells);
+            expect = [10 20;30 30;40 20];
+            testCase.verifyEqual(actual,expect);
         end
         
-        function testExpQueue( testCase )
+        function testDataRateAndQueueBase( testCase )
+            actual = UtilityFunctions.dataRateAndQueueBase(testCase.cells);
+            expect = [20 280;270 540;160 360];
+            testCase.verifyEqual(actual,expect);
         end
     end
     
