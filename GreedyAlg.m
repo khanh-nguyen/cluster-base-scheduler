002ce765d391241628860b069d0fe2cf14950e0f
Khanh.Nguyen <khanh.nguyen@opower.com>
2014-09-30 15:49:27 -0400

    Add unit tests for GreedyAlg

--- GreedyAlg.m
+++ GreedyAlg.m
@@ -2,13 +2,13 @@ classdef GreedyAlg < SchedulingAlgorithm
     % implement our greedy algorithm
     
     methods (Static)
-        function [ul, dl, p] = schedule(uplinks, downlinks, uProfit, dProfit, m)
+        function [ul, dl] = schedule(uplinks, downlinks, uProfit, dProfit, m)
             A = GreedyAlg.computeSubframeProfits(uplinks, uProfit);
             B = GreedyAlg.computeSubframeProfits(downlinks, dProfit);
-            [ul, dl, p] = GreedyAlg.merge(A, B, m);
+            [ul, dl] = GreedyAlg.merge(A, B, m);
         end
         
-        function results = computeSubframeProfits(links, profits) 
+        function pairs = computeSubframeProfits(links, profits) 
             %computeSubframeProfits returns an sorted array of tuples
             %  each tuple contains the profit and number of subframes with
             %  that amount of profit
@@ -17,52 +17,52 @@ classdef GreedyAlg < SchedulingAlgorithm
             %  profits - Nx1 column represents the profits for scheduling UL/DL subframe
             
             m = length(links);
+            pairs = zeros(m,2);
+            
+            % sort links, then sort profits accordingly
             [links, sortedIndex] = sort(links, 'descend');
             profits = profits(sortedIndex);
-            results = zeros(m,2);
             
-            idx = 1; i = 1; j = 2; a = profits(1); c = 0;
-            while (j <= length(links)) 
-                if (links(j) < links(i)) 
+            idx = 1; i = 1; j = 2; a = profits(1); 
+            while j <= m 
+                if links(j) < links(i) 
                     c = links(i) - links(j);
-                    results(idx,1) = a;
-                    results(idx,2) = c;
+                    pairs(idx,1) = a;
+                    pairs(idx,2) = c;
                     idx = idx + 1;
                     i = j;
-                    a = a + profits(i);
-                else
-                    a = a + profits(j);
                 end    
+                a = a + profits(j);
                 j = j + 1;
             end
             
             % handle the last subframe
-            results(idx, 1) = a;
-            results(idx, 2) = links(j-1);
+            pairs(idx, 1) = a;
+            pairs(idx, 2) = links(j-1);
             
             
-            % sort results in reverse order 
-            results = results(1:idx, :);
-            [~, index] = sort(results(:,1), 'descend');
-            results = results(index,:);
+            % truncate empty cells 
+            pairs = pairs(1:idx, :);
+            
+            % sort results in reverse order of profits
+            [~, index] = sort(pairs(:,1), 'descend');
+            pairs = pairs(index,:);
         end
         
-        function [ul, dl, p] = merge(U, D, m) 
+        function [a, b] = merge(A, B, m) 
             %merge merges array A and B and compute the optimal number of 
             %  uplinks and downlinks
             
-            ul = 0; dl = 0; p = 0;
+            a = 0; b = 0;
             i = 1; j = 1;
-            while (i <= size(U, 1) && j <= size(D, 1))
-                if (U(i,1) > D(j,1)) 
-                    c = min(U(i,2), m);
-                    ul = ul + c;
-                    p = p + U(i,1)*c;
+            while (i <= size(A, 1) && j <= size(B, 1) && m > 0)
+                if (A(i,1) > B(j,1)) 
+                    c = min(A(i,2), m);
+                    a = a + c;
                     i = i + 1;
                 else
-                    c = min(D(j,2), m);
-                    dl = dl + c;
-                    p = p + D(j,1)*c;
+                    c = min(B(j,2), m);
+                    b = b + c;
                     j = j + 1;
                 end
                 m = m - c;
---
+++ unit_test/TestGreedyAlg.m
@@ -0,0 +1,93 @@
+classdef TestGreedyAlg < matlab.unittest.TestCase
+    %UNTITLED12 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        alg;
+    end
+    
+    methods (TestMethodSetup)
+        function setup(testCase)
+            disp('settting up algorithm');
+            testCase.alg = GreedyAlg;
+        end
+    end
+    
+    methods (Test)
+        function testComputeSubframeProfits(testCase)
+            links = [7;4;4;4;3];
+            profits = [120;50;100;60;40];
+            actual = testCase.alg.computeSubframeProfits(links, profits);
+            expect = [370 3;330 1;120 3];
+            testCase.verifyEqual(actual, expect);
+            
+            links = [3;2;5;6;7];
+            profits = [100;80;40;70;60];
+            actual = testCase.alg.computeSubframeProfits(links, profits);
+            expect = [350 2;270 1;170 2;130 1;60 1];
+            testCase.verifyEqual(actual, expect);
+            
+            links = [3 2 5 6 7];
+            profits = [100 80 40 70 60];
+            actual = testCase.alg.computeSubframeProfits(links, profits);
+            expect = [350 2;270 1;170 2;130 1;60 1];
+            testCase.verifyEqual(actual, expect);
+            
+            links = [3 3 3 3];
+            profits = [40 50 10 20];
+            expect = [120 3];
+            actual = testCase.alg.computeSubframeProfits(links, profits);
+            testCase.verifyEqual(actual, expect);
+            
+            links = [1 2 3 4];
+            profits = [20 10 30 40];
+            actual = testCase.alg.computeSubframeProfits(links, profits);
+            expect = [100 1;80 1;70 1;40 1];
+            testCase.verifyEqual(actual, expect);
+            
+            links = [4 3 2 1];
+            profits = [20 10 30 40];
+            actual = testCase.alg.computeSubframeProfits(links, profits);
+            expect = [100 1;60 1;30 1;20 1];
+            testCase.verifyEqual(actual, expect);
+        end
+    
+        function testMerge(testCase)
+            A = [300 2;180 3;50 4];
+            B = [250 3;100 1;40 2];
+            m = 5;
+            [x, y] = testCase.alg.merge(A,B,m);
+            expect = [2 3];
+            testCase.verifyEqual([x, y],expect);
+            
+            m = 7;
+            [x, y] = testCase.alg.merge(A,B,m);
+            expect = [4 3];
+            testCase.verifyEqual([x, y],expect);
+        end
+        
+        function testSchedule(testCase) 
+            ul = [7;4;4;4;3];
+            dl = [3;2;5;6;7];
+            uP = [120;50;100;60;40];
+            dP = [100;80;40;70;60];
+            m = 10;
+            [x,y] = testCase.alg.schedule(ul,dl,uP,dP,m);
+            expect = [4 6];
+            testCase.verifyEqual([x,y],expect);
+        end
+        
+        function testSchedule2(testCase) 
+            ul = [3;3;5;2;2;1];
+            dl = [6;6;4;7;3;5];
+            uP = [40;30;10;40;30;20];
+            dP = [60;50;50;10;50;30];
+            m = 10;
+            [x,y] = testCase.alg.schedule(ul,dl,uP,dP,m);
+            expect = [3 7];
+            testCase.verifyEqual([x,y],expect);
+        end
+    end
+    
+end
+
